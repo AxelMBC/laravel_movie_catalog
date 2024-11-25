1f2be49dcfdc6ccbc006891f81fc2204044be670
3ARR4 <barrazaaxel2000@gmail.com>
2024-11-25 13:02:25 -0600

    updating

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
 
 Route::get('/catalogo', function () {
     return view('catalogo', ['tipo' => 'tecnologia']);
